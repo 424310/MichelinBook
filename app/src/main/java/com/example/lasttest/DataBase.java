d9ba38a38caa234f6c8c5e088ce2319dc73f4dde
424310 <weweq123@naver.com>
2019-05-21 07:21:58 +0900

    민지

--- app/src/main/java/com/example/lasttest/DataBase.java
+++ app/src/main/java/com/example/lasttest/DataBase.java
@@ -19,6 +19,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 
 public class DataBase extends AppCompatActivity implements View.OnClickListener{
+
 
     private DatabaseReference mPostReference;
 
